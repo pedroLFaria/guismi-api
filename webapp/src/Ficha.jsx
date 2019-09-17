485d7b512d95a71aa1d7b2d4bd574ad202a9d673
pedro.luz <pedro.luz@ibratan.com.br>
2019-09-17 19:43:04 -0300

    Fix na ficha

--- webapp/src/Ficha.jsx
+++ webapp/src/Ficha.jsx
@@ -33,16 +33,30 @@ class Caminho extends React.Component {
         this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange(event){
+    handleChange(event) {
+        if (this.state.ficha.caminhos.every(caminho => caminho.idCaminho !== Number(event.target.value))) {
+
         this.state.ficha.caminhos[event.target.id].idCaminho = Number(event.target.value);
-        this.setState({ficha:this.state.ficha});
+        this.setState({ficha: this.state.ficha});
         const xhr = new XMLHttpRequest();
         xhr.addEventListener('load', () => {
-           console.log("uploaded")
+            console.log("uploaded")
         });
         xhr.open('PUT', 'api/ficha/');
         xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xhr.send(JSON.stringify(this.state.ficha));
+        }else
+            alert("Você já tem este caminho")
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.ficha.caminhos.length !== this.props.ficha.caminhos.length){
+            this.setState({ficha:this.props.ficha})
+        }
+        for(let caminho in this.props.ficha.caminhos){
+            if(prevProps.ficha.caminhos.every(prevCaminho => prevCaminho.idCaminho !== this.props.ficha.caminhos[caminho].idCaminho))
+                this.setState({ficha:this.props.ficha})
+        }
     }
 
     render() {
@@ -84,30 +98,69 @@ class Raca extends React.Component {
 
     componentDidUpdate(prevProps){
         if(prevProps.ficha.idRaca !== this.props.ficha.idRaca)
-            this.setState({value:this.props.ficha.idRaca})
+            this.setState({ficha:this.props.ficha})
     }
 
     render() {
         return (
-            <select value={this.props.ficha.idRaca} onChange={this.handleChange} className="form-control">
+            <select value={this.state.ficha.idRaca} onChange={this.handleChange} className="form-control">
                 {this.state.sistema.racas.map((raca) => <option value={raca.idRaca}>{raca.nomeRaca}</option>)}
             </select>
         )
     }
 }
 
+class Nome extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            ficha: this.props.ficha
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.putFicha = this.putFicha.bind(this);
+    }
+
+    handleChange(event) {
+        this.state.ficha.nomePersonagem = event.target.value;
+        this.setState({ficha:this.state.ficha});
+        this.render();
+    }
+
+    putFicha(){
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', () => {
+        });
+        xhr.open('PUT', 'api/ficha/');
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send(JSON.stringify(this.state.ficha));
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps.ficha.nomePersonagem !== this.props.ficha.nomePersonagem)
+            this.setState({ficha:this.props.ficha})
+    }
+
+    render(){
+        return (
+            <input type="text" value={this.state.ficha.nomePersonagem}  onChange={this.handleChange} onBlur={this.putFicha} className="c form-control"
+                   placeholder="" id="nome_pers_input"/>
+        )
+    }
+}
+
 class Ficha extends React.Component {
     constructor(props) {
-        console.log("constructor");
         super(props);
+        this.tick();
         this.state = {
             ficha: {caminhos: [], descendencias: [], habilidades: [], habitos: [], idiomas: [], inventarios: [], patronos: [], raca: {}, situacoes: []},
             sistema: JSON.parse(sessionStorage.getItem("sistema"))
-        }
+        };
+
     };
 
     componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), 5000);
+        this.timerID = setInterval(() => this.tick(), 3000);
     };
 
     tick() {
@@ -118,13 +171,6 @@ class Ficha extends React.Component {
                 sessionStorage.setItem("ficha", JSON.stringify(data));
                 this.setState({ficha: data})
             })
-    }
-
-    componentDidUpdate(prevProps){
-        console.log("prevProps: " + prevProps);
-        console.log("this props: "+ this.props)
-        if(prevProps.ficha.idFicha !== this.props.ficha.idFicha)
-            this.setState({ficha:this.props.ficha})
     }
 
     render() {
@@ -139,8 +185,10 @@ class Ficha extends React.Component {
                     <div className="row col-md-12">
                         <div className="col-md-2 inline">
                             <span>Nome</span>
-                            <input type="text" value={this.state.ficha.nomePersonagem} className="c form-control"
-                                   placeholder="" id="nome_pers_input"/>
+                            <Nome
+                                ficha={ficha}
+                                sistema={sistema}
+                                />
                         </div>
                         <div className="col-md-2">
                             <span>Raça</span>
