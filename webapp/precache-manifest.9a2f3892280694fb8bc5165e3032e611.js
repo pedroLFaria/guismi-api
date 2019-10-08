2c1b28a1ee666b6c7355970413b184038a3f2741
pedro.luz <pedro.luz@ibratan.com.br>
2019-10-08 10:21:38 -0300

    Adicionando build react.

---
+++ webapp/precache-manifest.9a2f3892280694fb8bc5165e3032e611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc09a3c0ca0c91b4a7a8fd23a629ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5d3ae6be160312cd7f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2e35dfaeff1c2f8b83f9",
+    "url": "/static/css/main.c484bbc1.chunk.css"
+  },
+  {
+    "revision": "5b5d3ae6be160312cd7f",
+    "url": "/static/js/2.78a9c268.chunk.js"
+  },
+  {
+    "revision": "2e35dfaeff1c2f8b83f9",
+    "url": "/static/js/main.fc69d76f.chunk.js"
+  },
+  {
+    "revision": "2e3934f90f518e1b78d4",
+    "url": "/static/js/runtime~main.989a2668.js"
+  }
+]);
